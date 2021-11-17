9e71a73998e8db7479f1a1fa7c936bd27cea8bcc
Yuval sandler <sandler2806@gmai.com>
2021-11-17 13:43:20 +0200

    constructor changed

--- building.py
+++ building.py
@@ -1,17 +1,11 @@
-
-import copy
 import json
 
+
 class Building:
-    def __init__(self,fname):
-        with open(fname,"r") as f:
-            dict = json.load(f)
-        self.minFloor = dict.get('_minFloor')
-        self.maxFloor = dict.get('_maxFloor')
-        self.Elevators = dict.get('_elevators')
-
-
-
-
-
-
+    def __init__(self, file_name):
+        if file_name != "":
+            with open(file_name, "r") as f:
+                dict2 = json.load(f)
+            self.minFloor = dict2.get('_minFloor')
+            self.maxFloor = dict2.get('_maxFloor')
+            self.Elevators = dict2.get('_elevators')
