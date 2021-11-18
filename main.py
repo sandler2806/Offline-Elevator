aec2d3cf7932599596f635cc46c8eb41dc0072d9
Yuval sandler <sandler2806@gmai.com>
2021-11-18 09:13:58 +0200

    separate classes

--- main.py
+++ main.py
@@ -1,266 +1,48 @@
-import copy
-import sys
-
+from simulator_tools import *
 import pandas as pd
-from building import Building
-import math
-
-# efdfg
-
-building = []
-calls = []
-
-
-# outfile =  input("insert a output file path")
-
-# this function receive a up to 5 calls and assign them to the elevators
-def allocate(callsList, indices: [[]]):
-    global calls
-    # initial_copys = copy.deepcopy(calls)
-    # for i in range(0,len(initial_copys)):
-
-    elevatorNum = len(building.Elevators)
-    copys = [[]] * elevatorNum
-    minWaitingTime = 10000000000
-    minSetup = []
-    for i in indices:
-        for j in set(i):
-            copys[j] = calls[j]
-        row = 0
-        for j in i:
-            insert_call(copys[j], j, callsList[row][2], callsList[row][3], math.ceil(callsList[row][1]))
-            row += 1
-        time = 0
-        for j in range(elevatorNum):
-            if j in i:
-                time += timeCalculator(copys[j], j)
-            else:
-                time += timeCalculator(calls[j], j)
-        if time < minWaitingTime:
-            minSetup = i
-            minWaitingTime = time
-    row = 0
-    for m in minSetup:
-        insert_call(calls[m], m, callsList[row][2], callsList[row][3], math.ceil(callsList[row][1]))
-        row += 1
-
-    return minSetup
-
-
-# this function gets an elevator,src and dest of a new call, and return
-# the delayed caused by assigning this elevator for the new call
-# def delayCalculator(calls,elev, src, dest):
-#     pass
-
-# this function get elevator and it's stops and calculate the time
-# it take to complete them
-def timeCalculator(stops: [[]], elev):
-    time = 0  # total waiting time of all the users
-    onBoard = 0  # number of people(calls) on board
-    elevator = building.Elevators[elev]
-    # index = len(stops) - 1
-    # for i in range(len(stops) - 1, -1, -1):
-    #     if stops[i][2] < time_check:
-    #         index = i
-    #         break
-    for i in range(1, len(stops)):
-        # add the traveling time times the travelers onboard
-        time += onBoard * (stops[i][2] - stops[i - 1][2])
-        # add the time users in stop i waited for the elevator
-        for boardingTime in stops[i][3]:
-            time += stops[i][2] - boardingTime
-
-        onBoard += stops[i][1]
-
-    return time
-
-
-def getPos(stops, elev, time):
-    time = math.ceil(time)
-
-    index = 0
-    minBoardingTime = 10000000000
-    travelersCounter = 0
-    elevator = building.Elevators[elev]
-    speed = elevator["_speed"]
-    # we look for the
-    for i in range(len(stops) - 1, -1, -1):
-        if len(stops[i][3]) > 0:
-            minBoardingTime = min([min(stops[i][3]), minBoardingTime])
-        if stops[i][2] < time:
-            index = i
-            break
-        travelersCounter += stops[i][1]
-
-    # we check 2 things:
-    # 1)is there someone currently waiting for the elevator
-    isWaiting = (minBoardingTime <= stops[index][2])
-    # 2)is there someone on the elevator
-    isOnElev = False
-    if len(stops[index][3]) == 0 and travelersCounter < 0:
-        isOnElev = True
-
-    if isWaiting or isOnElev:
-        departingTime = stops[index][2]
-    else:
-        departingTime = minBoardingTime
-
-    t = time - departingTime - (elevator["_closeTime"] + elevator["_startTime"])
-
-    if t <= 0:
-        return [stops[index][0], abs(t)]
-
-    return [min(stops[index][0] + t * speed, stops[index + 1][0]), 0]
-
-
-# this function insert a call to the stop list of given elevator by Boaz logic
-# while taking into account the boarding time
-
-def insert_call(stops: [[[]]], elev, src, dest, time):
-    elevator = building.Elevators[elev]
-    floor_time = elevator["_closeTime"] + elevator["_openTime"] + elevator["_startTime"] + elevator["_stopTime"]
-    speed = elevator["_speed"]
-    if len(stops) == 1:
-
-        if src != 0:  # the first src is already 0
-            stops.append([src, 1, time + abs(src) / speed + floor_time, [time]])
-        else:
-            stops.append([src, 1, time, [time]])
-        stops.append([dest, -1, math.ceil(abs(dest - src) / speed + floor_time + stops[1][2]), []])
-
-    else:
-        index = len(stops) - 1
-        srcIndex = 0
-        srcTime = 0
-        destTime = 0
-        destIndex = 0
-        minBoardingTime = time
-        for i in range(len(stops) - 1, -1, -1):
-            # if len(stops[i][3]) > 0:
-            #    minBoardingTime = min([min(stops[i][3]), minBoardingTime])
-            if stops[i][2] < time:
-                index = i
-                break
-
-        for i in range(index, len(stops)):
-            pos = getPos(stops, elev, time)  # check order
-            # call added to the end of the list
-            if i == len(stops) - 1:
-                if src == stops[i]:
-                    srcIndex = i
-                    stops[i][1] += 1
-                    stops[i][3].append(time)
-                    break
-                else:
-                    srcIndex = i + 1
-                    srcTime = math.ceil(abs(stops[i][0] - src) / speed + floor_time + max(stops[i][2], time))
-                    stops.append([src, 1, srcTime, [time]])
-                    break
-            # special test for the first interval
-            if i == index and (pos[0] <= src <= stops[i + 1][0] or pos[0] >= src >= stops[i + 1][0]):
-                if src == pos[0] and pos[0] == stops[i][0] and pos[1] == 0:
-                    srcIndex = i
-                    stops[i][1] += 1
-                    stops[i][3].append(time)
-                elif src == stops[i + 1][0]:
-                    srcIndex = i + 1
-                    stops[i + 1][1] += 1
-                    stops[i + 1][3].append(time)
-                else:
-                    srcIndex = i + 1
-                    # warning: adding time might be an overfitting
-                    srcTime = math.ceil(
-                        max(stops[i][2], time) + pos[1] + abs(pos[0] - src) / speed + elevator["_openTime"] + \
-                        elevator["_stopTime"])
-                    stops.insert(srcIndex, [src, 1, srcTime, [time]])
-                    # adding delay caused from the new stop
-                    for p in range(srcIndex + 1, len(stops)):
-                        stops[p][2] += floor_time
-                break
-
-            if stops[i][0] <= src <= stops[i + 1][0] or stops[i][0] >= src >= stops[i + 1][0]:
-                if src == stops[i][0]:
-                    srcIndex = i
-                    stops[i][1] += 1
-                    stops[i][3].append(time)
-                elif src == stops[i + 1][0]:
-                    srcIndex = i + 1
-                    stops[i + 1][1] += 1
-                    stops[i + 1][3].append(time)
-                else:
-                    srcIndex = i + 1
-                    srcTime = math.ceil(abs(stops[i][0] - src) / speed + floor_time + max(stops[i][2], time))
-                    stops.insert(srcIndex, [src, 1, srcTime, [time]])
-                    # adding delay caused from the new stop
-                    for p in range(srcIndex + 1, len(stops)):
-                        stops[p][2] += floor_time
-                break
-
-        for i in range(srcIndex, len(stops)):
-            if i == len(stops) - 1:
-                destTime = math.ceil(abs(stops[i][0] - dest) / speed + floor_time + stops[i][2])
-                stops.append([dest, -1, destTime, []])
-                break
-
-            if stops[i][0] <= dest <= stops[i + 1][0] or stops[i][0] >= dest >= stops[i + 1][0]:
-                if dest == stops[i][0]:
-                    stops[i][1] -= 1
-                elif dest == stops[i + 1][0]:
-                    stops[i + 1][1] -= 1
-                else:
-                    destTime = math.ceil(abs(stops[i][0] - dest) / speed + floor_time + stops[i][2])
-                    stops.insert(i + 1, [dest, -1, destTime, []])
-                    # adding delay caused from the new stop
-                    for p in range(i + 2, len(stops)):
-                        stops[p][2] += floor_time
-                break
-
-
-def decToBaseX(call_num, base, callSize):
-    reminder = []
-    for i in range(callSize):
-        reminder.insert(0, call_num % base)
-        call_num = math.floor(call_num / base)
-
-    return reminder
+import sys
 
 
 def main(argv):
-    global building
-    global calls
     building = Building(argv[0])
     calls = [[[0, 0, 0, []]] for q in range(len(building.Elevators))]
     callsfile = argv[1]
     output = argv[2]
+    callSize = 1
+    counter = 0
+    indices = []
 
     df = pd.read_csv(callsfile, header=None)
     rows = [r[1] for r in df.iterrows()]
 
-    callSize = 1
-    counter = 0
-    temp = []
-    indices = []
     for i in range(pow(len(building.Elevators), callSize)):
         indices.append(decToBaseX(i, len(building.Elevators), callSize))
+    callsList = []
     for c in rows:
-        temp.append(c)
-        if len(temp) == callSize:
-            if counter == 99:
-                print("h")
+        callsList.append(c)
 
-            assignments = allocate(temp, indices)
+        if len(callsList) == callSize:
+            json_list = []
+            for j in range(len(calls)):
+                json_list.append(json.dumps(calls[j]))
+
+            calls, assignments = allocate(callsList, indices, json_list, building, calls)
+            l = []
+            l = list(callsList[0])
             for a in assignments:
                 df[5][counter] = a
                 counter += 1
-            temp = []
+            callsList = []
 
-    if len(temp) > 0:
-        assignments = allocate(temp, callSize)
+    json_list = []
+    for j in range(len(calls)):
+        json_list.append(json.dumps(calls[j]))
+    if len(callsList) > 0:
+        calls, assignments = allocate(callsList, indices, json_list, building, calls)
         for a in assignments:
             df[5][counter] = a
             counter += 1
 
-    print(df)
     df.to_csv(output, index=None, header=False)
 
 
--- tests.py
+++ tests.py
@@ -1,46 +1,68 @@
 import unittest
 
 from main import *
-# from building import Building
 
 
 class mainTest(unittest.TestCase):
 
-    b1 = Building("f.json")
-
-    def test_something(self):
-        # self.assertEqual(True, False)  # add assertion here
-        self.assertFalse(len([]) != 0)
-
+    def test_allocate(self):
+        b1 = Building("f.json")
+        calls= [[[0, 0, 0, []]] for q in range(len(b1.Elevators))]
+        callsList=[['Elevator call', 15.74901825, 0, -6, 0, -1]]
+        callsList1 = [['Elevator call', 24.346, 12, 80, 0, -1]]
+        callsList2 = [['Elevator call', 53.1423, -3, 42, 0, -1]]
+        json_List=['[[0, 0, 0, []]]', '[[0, 0, 0, []]]']
+        c, ans = allocate(callsList, [[0], [1]], json_List, b1, calls)
+        self.assertTrue([0] == ans)
+        c, ans1 = allocate(callsList, [[1], [1]], json_List, b1, calls)
+        self.assertTrue([1] == ans1)
+        c, ans2 = allocate(callsList1, [[0], [1]], json_List, b1, calls)
+        self.assertTrue([0] == ans2)
+        c, ans3 = allocate(callsList2, [[0], [0]], json_List, b1, calls)
+        self.assertTrue([0] == ans3)
 
     def test_timeCalc(self):
-        lst = [[0, 1, 1, [1]], [0, 1, 11, [3]], [-1, -1, 22, [19]], [-2, -1, 33, []], [0, 1, 45, [30]], [1, -1, 56, []]]
+        lst = [[0, 0, 0], [0, 1, 1, [1]], [0, 1, 11, [3]], [-1, -1, 22, [19]], [-2, -1, 33, []], [0, 1, 45, [30]],
+               [1, -1, 56, []]]
+        lst2 = [[0, 0, 0, []], [-7, 1, 44, [31]], [-6, 1, 55, [47]], [-4, 4, 74, [63, 68, 68, 86]]]
+        lst3 = [[0, 0, 0, []], [0, 1, 16, [16]], [-6, -1, 24, []], [-5, 1, 38, [31]], [-4, 2, 46, [30, 53]]]
+        lst4 = [[0, 0, 0, []], [-2, 0, 187, [177]], [-1, -2, 195, [182]], [0, 1, 203, [166, 199]], [9, -1, 212, []]]
 
-        print(timeCalculator(lst, 0))
         self.assertTrue(80 == timeCalculator(lst, 0))
+        self.assertTrue(81 == timeCalculator(lst2, 0))
+        self.assertTrue(32 == timeCalculator(lst3, 0))
+        self.assertTrue(39 == timeCalculator(lst4, 0))
 
     def test_getPos(self):
-        lst = [[0, 1, 1, [1]], [0, 1, 11, [3]], [-1, -1, 22, [19]], [-2, -1, 33, []], [0, 1, 45, [30]], [1, -1, 56, []]]
-        print(getPos(lst, 0, 50))
-        self.assertTrue(-2 == getPos(lst, 0, 35)[0])
+        b1 = Building("f.json")
+
+        lst = [[0, 0, 0], [0, 1, 1, [1]], [0, 1, 11, [3]], [-1, -1, 22, [19]], [-2, -1, 33, []], [0, 1, 45, [30]],
+               [1, -1, 56, []]]
+        lst2 = [[0, 0, 0, []], [-7, 1, 44, [31]], [-6, 1, 55, [47]], [-4, 4, 74, [63, 68, 68, 86]]]
+        lst3 = [[0, 0, 0, []], [0, 1, 16, [16]], [-6, -1, 24, []], [-5, 1, 38, [31]], [-4, 2, 46, [30, 53]]]
+        lst4 = [[0, 0, 0, []], [-2, 0, 187, [177]], [-1, -2, 195, [182]], [0, 1, 203, [166, 199]], [9, -1, 212, []]]
+
+        self.assertTrue(-2 == getPos(lst, 0, 35, b1)[0])
+        self.assertTrue(-5 == getPos(lst2, 0, 61, b1)[0])
+        self.assertTrue(-4 == getPos(lst3, 0, 44, b1)[0])
+        self.assertTrue(0 == getPos(lst4, 0, 205, b1)[0])
 
     def test_insert(self):
+        b1 = Building("f.json")
 
-        initial = []
-        insert_call(initial, 0, 0, -1, 1)
-        insert_call(initial, 0, 0, -1, 3)
-        insert_call(initial, 0, -1, -2, 19)
-        insert_call(initial, 0, 0, 1, 30)
-        insert_call(initial, 0, 2, 1, 70)
-        print(initial)
+        initial = [[0, 0, 0]]
+        insert_call(initial, 0, 0, -1, 1, b1)
+        insert_call(initial, 0, 0, -1, 3, b1)
+        insert_call(initial, 0, -1, -2, 19, b1)
+        insert_call(initial, 0, 0, 1, 30, b1)
+        insert_call(initial, 0, 2, 1, 70, b1)
 
     def test_baseX(self):
-        print(decToBaseX(0, 4, 5))
+        self.assertTrue(decToBaseX(0, 4, 5) == [0, 0, 0, 0, 0])
+        self.assertTrue(decToBaseX(11, 3, 5) == [0, 0, 1, 0, 2])
+        self.assertTrue(decToBaseX(20, 2, 5) == [1, 0, 1, 0, 0])
+        self.assertTrue(decToBaseX(30, 4, 5) == [0, 0, 1, 3, 2])
 
 
-
-
-
-
-if __name__ == '__main__':
+if _name_ == '_main_':
     unittest.main()
